da22dad0ff7f5906ff42bad749a7fba0cfc96b3a
EwwPhysics <74149971+EwwPhysics@users.noreply.github.com>
2021-09-18 17:50:15 -0400

    CSV created by superscore.py sorts by total points.

--- results/superscore.py
+++ results/superscore.py
@@ -9,11 +9,11 @@ def write_placements(scores, events, name) -> None:
         os.makedirs(directory)
 
     with open(path, "w", newline="") as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames=["School"] + events)
+        writer = csv.DictWriter(csvfile, fieldnames=["School"] + events + ["Total"])
         writer.writeheader()
 
-        for school in scores:
-            writer.writerow({"School": school} | (scores[school]))
+        for school in sorted(scores, key=lambda x: sum(scores[x].values())):
+            writer.writerow({"School": school} | (scores[school]) | {"Total": sum(scores[school].values())})
 
 def main(scores, events, name):
     write_placements(scores, events, name)
