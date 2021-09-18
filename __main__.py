24365e338c0f617d860e2f384af7c1397f948563
EwwPhysics <74149971+EwwPhysics@users.noreply.github.com>
2021-09-18 17:41:54 -0400

    Make argument parse partially functional
    Still WIP oop

--- __main__.py
+++ __main__.py
@@ -1,4 +1,4 @@
-from results import graph, scilympiad, sciolyFF, spreadsheet
+from results import graph, superscore, parse_link, download_yaml
 import argparse
 
 parser = argparse.ArgumentParser()
@@ -10,3 +10,15 @@ m = args.module
 link = args.file
 
 file, events, name = parse_link.parse(link)
+
+if m == "superscore":
+    scores = parse_link.superscore(file, link)
+    superscore.main(scores, events, name)
+    print(f"Superscored CSV can be found in ./csv_out/{name}")
+elif m == "download":
+    download_yaml.main(file, name, link)
+elif m == "graph":
+    #TODO: do this :P
+    pass
+else:
+    raise argparse.ArgumentTypeError("__main__.py: error: module must be 'superscore,' 'graph,' or 'spreadsheet'")
