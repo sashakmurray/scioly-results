9c1fdeef87dfa3e916d0d03cb248f206a209f34b
EwwPhysics <74149971+EwwPhysics@users.noreply.github.com>
2021-09-18 17:36:29 -0400

    Create an argument parser
    Accepts arguments; doesn't do anything with them yet.

--- __main__.py
+++ __main__.py
@@ -1,5 +1,12 @@
 from results import graph, scilympiad, sciolyFF, spreadsheet
 import argparse
 
-print("hello world")
+parser = argparse.ArgumentParser()
+parser.add_argument("file")
+parser.add_argument("-m", dest="module")
+args = parser.parse_args()
 
+m = args.module
+link = args.file
+
+file, events, name = parse_link.parse(link)
